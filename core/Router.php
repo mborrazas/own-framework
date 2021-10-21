2c3fb461fbe78590fa33bf972a7caa6dfb034633
Matias Borrazas <borrazas.trabajo@gmail.com>
2021-10-21 02:20:13 -0300

    Add routes

---
+++ core/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\core;
+
+class Router {
+    protected array $routes = [];
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+}
