03c0677e0573ea66593db153567a656c9bc68b3a
Matias Borrazas <borrazas.trabajo@gmail.com>
2021-10-21 03:16:52 -0300

    Add Request, Response and method POST

--- core/Router.php
+++ core/Router.php
@@ -2,16 +2,20 @@
 
 namespace app\core;
 
-class Router {
+
+class Router
+{
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
     /**
      * @param Request $request
      */
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -19,15 +23,46 @@ class Router {
         $this->routes['get'][$path] = $callback;
     }
 
-    public function resolve(){
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
-        if($callback === false){
-            echo "Not found";
+        if ($callback === false) {
+            return "Not found";
             exit;
         }
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            $this->response->setStatusCode(404);
+            return $this->renderView($callback);
+        }
+        return call_user_func($callback);
+    }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+        return ob_get_clean();
     }
 
 
